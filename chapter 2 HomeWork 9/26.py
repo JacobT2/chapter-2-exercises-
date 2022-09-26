ba84128c25387ab96f79c41fa6022292873f41c8
jacobaleixo <jtaleixo@my.ccri.edu>
2022-09-26 19:53:16 -0400

    test #2 commit

---
+++ chapter 2 HomeWork 9/26.py
@@ -0,0 +1,84 @@
+# #Qestion 1
+# n = input("Your name? ")
+# a = input("Your address, with city, state, and ZIP? ")
+# p = input("Your telephone number? ")
+# c = input("Your college major?" )
+#
+# print ("Your address, with city, state, and ZIP: ", a)
+# print("Name: " , n)
+# print("Your telephone number: ", p)
+# print("Your college major: ", c)
+#
+# #Question 2
+# s = int(input("What where your total sales today? "))
+# p = s * 0.23
+# print("Your profit is: ", p, "$")
+
+#Question3
+# f = int(input("How many feet are in your property? "))
+# a = f/43560
+# print("You have", a, "acres of land. ")
+
+#Question4
+# i = float(input("How much are your items? "))
+# t = i*1.07
+# print("with taxes your total will be", t , "$")
+#Question 5
+# s = 70
+# a = s * 6
+# b = s * 10
+# c = s * 15
+# print("Your distance is", a , "miles.")
+# print("Your distance is", b , "miles.")
+# print("Your distance is", c , "miles.")
+#Question 6
+# i = float(input("How much are your items? "))
+# st = i*1.025
+# t = i * 1.05
+# tst = i * 1.075
+# print("with sate taxes your total will be", t , "$")
+# print("with sale taxes your total will be", st , "$")
+# print("with taxes your total will be", tst , "$")
+#Question 7
+# m = int(input("How many miles have you driven? "))
+# g = int(input("How many gallons diud you used? "))
+# MPG = m/g
+# print("Your car gets", MPG , "MPG")
+#Question 8
+# c = float(input("How much much was the bill? "))
+# t = c * 0.18
+# st = c * 0.07
+# print("the tip will be", t , "$")
+# print("the tax will be", st , "$")
+# t2 = c * 1.18
+# st2 = c * 1.07
+# nt = t2+st2
+# print("the total will be", nt ,"$")
+# Question 9
+# t = int(input("Whats the tempetrure in Celsius? "))
+# f = 9/5 * t + 32
+# print("The temperture in Fahrenheit is:" , f)
+#Question 10
+# c = int(input("How many cookies would you like to make? "))
+# s = c *1.5  #cups of sugar
+# b = c * 1 #cup of butter
+# f = c * 2.75 #cups of flour
+# print("You will need", s , "cups of suger.")
+# print("You will need", b , "cups of butter.")
+# print("You will need", f , "cups of flower.")
+
+#Question 12
+# S = 2000 #The number of shares that Joe purchased was 2,000.
+# IP = 40 * S #When Joe purchased the stock, he paid $40.00 per share.
+# SF = IP * 0.03 #Joe paid his stockbroker a commission that amounted to 3 percent of the amount he paid
+# print("Joe paid", S ,"$ for the stocks.")
+# print("Joe also had to pay", SF ,"$ to the broker.")
+# FP = 42.75 * S #Price joe sold the stocks
+# print("Joe sold his stocks for", FP ,"$")
+# FF = FP * 0.03 #Fee joe had to pay to the broker when he sold
+# print("Joe had to pay another comision fee of", FF , "$ to the broker.")
+# MLO = (IP + SF + FF) - FP
+# if  (MLO > 0):
+#      print("Joe had a posstive amount")
+# else:
+#      print("Joe had a negative amount")
