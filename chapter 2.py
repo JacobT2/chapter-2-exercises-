fa2db57b6095f747c5f906b645cedc35f9687b49
jacobaleixo <jtaleixo@my.ccri.edu>
2022-09-26 19:32:21 -0400

    test #2 commit

---
+++ chapter 2.py
@@ -0,0 +1,55 @@
+#Q1
+#hight = input("How tall are you? ")
+#print("You are", hight)
+#Q2
+#color = input("What's your favrite color? ")
+#print("I also love the color",color)
+#Q3
+#number = int(input("Give me a number from 1-10 "))
+#Number = number + 2
+#Number = number * 2
+#Number = number / 3.14
+#Number = number - 8
+#print(Number)
+#Q4
+#w = 5
+#x = 4
+#y = 8
+#z = 2
+#result1 = x + y
+#result2 = z * 2
+#result3 = y // x
+#result4 = y – z
+#result5 = w // z
+#print(result)
+#Q5
+#number = 10 + 14
+#print(number)
+#Q6
+#down_payment = int(input("You owe 10,000$, how much would you want to do as a down payment? "))
+#owe = 10000 - down_payment
+#print(owe)
+#Q7
+#N = float(input("give me anumber 1-10: "))
+#value = N *0.15
+#print(value)
+#Q8
+#a = 5
+#b = 2
+#c = 3
+#result = a + b * c
+#print(result)
+#Q9
+#5
+#Q10
+#n = 1234567.456
+#print(f'The number is {n:12,.2f}')
+#n = 11
+#N= n/ 2
+#print(N)
+#n = 1234567.456
+#print(f'The number is {n:12,.1f}')
+#Q12
+#print('George', 'John', 'Paul', 'Ringo', sep='@')
+#George@John@Paul@Ringo
+
