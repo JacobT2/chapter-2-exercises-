4ab975561174e1b9360f1e27ffb6dae75d0b395b
jacobaleixo <jtaleixo@my.ccri.edu>
2022-10-03 19:53:04 -0400

    test #3 commit

---
+++ chapter 2 HomeWork 9/chapter 3 classWork.py
@@ -0,0 +1,35 @@
+# Q1
+# x = int(input("Give me ab=value for x: "))
+# y = 20
+# if x > 20:
+#     x = 100
+# print("x:" ,x)
+# Q2
+# c = int(input("Give me a value for c: "))
+# b = 0
+# if c < 10:
+#     c = 1
+# print("x:" ,c)
+# Q3
+# b = int(input("Give me a value for b: "))
+#
+# if b < 10:
+#     b = 0
+# else:
+#     b = 99
+# print("b:" ,b)
+# Q4
+score = int(input("Whats was your score? "))
+if score >= A_score:
+print('Your grade is A.')
+else:
+if score >= B_score:
+print('Your grade is B.')
+else:
+if score >= C_score:
+print('Your grade is C.')
+else:
+if score >= D_score:
+print('Your grade is D.')
+else:
+print('Your grade is F.')
