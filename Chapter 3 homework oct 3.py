3558b9658227a2e067c2f6cbce5c003835357e83
jacobaleixo <jtaleixo@my.ccri.edu>
2022-10-04 11:27:41 -0400

    question 9

--- Chapter 3 homework oct 3.py
+++ Chapter 3 homework oct 3.py
@@ -52,18 +52,43 @@
 #     print("Too light")
 # Q7
 # List the primary colors
-print("The primiry color are: Red, Blue, and Yellow ")
-color1 = input("whats the first color would you like to mix? ")
-color2 = input("whats the second color would you like to mix? ")
-
-#When you mix red and blue, you get purple.
-#When you mix red and yellow, you get orange.
-#When you mix blue and yellow, you get green.
-if color1 == "red" and color2 == "blue":
-    print("You made purple!")
-elif color1 == "red" and color2 == "yellow":
-    print("You made orange!")
-elif color1 == "blue" and color2 == "yellow":
-    print("You made green!")
-else:
-    print("Please choise a primary color")
+# print("The primiry color are: Red, Blue, and Yellow ")
+# color1 = input("whats the first color would you like to mix? ")
+# color2 = input("whats the second color would you like to mix? ")
+#
+# #When you mix red and blue, you get purple.
+# #When you mix red and yellow, you get orange.
+# #When you mix blue and yellow, you get green.
+# if color1 == "red" and color2 == "blue":
+#     print("You made purple!")
+# elif color1 == "red" and color2 == "yellow":
+#     print("You made orange!")
+# elif color1 == "blue" and color2 == "yellow":
+#     print("You made green!")
+# else:
+#     print("Please choise a primary color")
+# # Q9
+# # first ask for the input
+# value = int(input("Give me a numerical number from 0-36: "))
+# # evaluate the input
+# if value == 0:
+#     print("The color is green")
+# elif value == 1 or value == 3 or value == 5 or value == 7 or value == 9:
+#     print("The color is red")
+# elif value == 2 or value == 4 or value == 6 or value == 8 or value == 10:
+#     print("The color is black")
+# elif value == 11 or value == 13 or value == 15 or value == 17:
+#     print("The color is black")
+# elif value == 12 or value == 14 or value == 16 or value == 18:
+#     print("The color is red")
+# elif value == 19 or value == 21 or value == 23 or value == 25 or value == 27:
+#     print("The color is red")
+# elif value == 20 or value == 22 or value == 24 or value == 26 or value == 28:
+#     print("The color is black")
+# elif value == 29 or value == 31 or value == 33 or value == 35:
+#     print("The color is black")
+# elif value == 30 or value == 32 or value == 34 or value == 36:
+#     print("The color is red")
+# # if they choise the right number
+# else:
+#     print(f"The input value '{value}' isnt valid Please choise a numerical number from 0 = 36")
