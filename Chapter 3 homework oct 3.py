ce8fdc7655fa7f40bf00ce726ec2380a340eb6a3
jacobaleixo <jtaleixo@my.ccri.edu>
2022-10-03 21:29:55 -0400

    question3

--- Chapter 3 homework oct 3.py
+++ Chapter 3 homework oct 3.py
@@ -25,3 +25,19 @@
 #     print("Sunday")
 # else:
 #     print(f"The input value {day} isnt valid. Please enter a numerical number 1-7")
+# Q3
+# # input age to evaluate
+age = int(input("What is your age? "))
+# If the person is 1 year old or less, he or she is an infant.
+# If the person is older than 1 year, but younger than 13 years, he or she is a child.
+#If the person is at least 13 years old, but less than 20 years old, he or she is a teenager.
+#If the person is at least 20 years old, he or she is an adult.
+# if age <= 12:
+#     print("You are an infint")
+# elif age > 1 and age < 13:
+#     print("You are a child. ")
+# elif age >= 13 and age < 20:
+#     print("You are a teen. ")
+# elif age >= 20:
+#     print("You are an adult. ")
+# Q5
