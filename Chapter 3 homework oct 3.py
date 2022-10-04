d0754cd2e74c868d00e2e7eb95cc91797351bd5a
jacobaleixo <jtaleixo@my.ccri.edu>
2022-10-03 21:21:41 -0400

    question1

---
+++ Chapter 3 homework oct 3.py
@@ -0,0 +1,27 @@
+# Q1
+#Gather value to evaluate
+#day = int(input("input a numerical number 1-7: "))
+# Mon = 1
+# tues = 2
+# wens = 3
+# thu = 4
+# fri= 5
+# sat = 6
+# sun = 7
+#
+# if day == 1:
+#     print("Monday")
+# elif day == 2:
+#     print("Tuesday")
+# elif day == 3:
+#     print("Wesnday")
+# elif day == 4:
+#     print("Thurday")
+# elif day == 5:
+#     print("Friday")
+# elif day == 6:
+#     print("Saturday")
+# elif day == 7:
+#     print("Sunday")
+# else:
+#     print(f"The input value {day} isnt valid. Please enter a numerical number 1-7")
