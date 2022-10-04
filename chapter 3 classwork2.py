ab42442ec7ba4d99583c856ec1d1cb480f78b689
jacobaleixo <jtaleixo@my.ccri.edu>
2022-10-03 20:52:32 -0400

    classWork for Oct 3

--- chapter 3 classwork2.py
+++ chapter 3 classwork2.py
@@ -1,35 +1,80 @@
 # Q1
+#Gather input
 # x = int(input("Give me ab=value for x: "))
 # y = 20
 # if x > 20:
 #     x = 100
 # print("x:" ,x)
 # Q2
+#Gather input
 # c = int(input("Give me a value for c: "))
 # b = 0
 # if c < 10:
 #     c = 1
 # print("x:" ,c)
 # Q3
+#Gather input
 # b = int(input("Give me a value for b: "))
-#
+#make sure b fits follows the rules
 # if b < 10:
 #     b = 0
 # else:
 #     b = 99
 # print("b:" ,b)
 # Q4
-score = int(input("Whats was your score? "))
-if score >= A_score:
-print('Your grade is A.')
-else:
-if score >= B_score:
-print('Your grade is B.')
-else:
-if score >= C_score:
-print('Your grade is C.')
-else:
-if score >= D_score:
-print('Your grade is D.')
-else:
-print('Your grade is F.')
+#Gather input
+# score = int(input("Whats was your score? "))
+# what each number is in letter
+# A_score = 90
+# B_score = 80
+# C_score = 70
+# D_score = 60
+#
+# find what value fits yur score
+# if score >= A_score:
+#     print("Your grade is A.")
+# else:
+#     if score >= B_score:
+#         print('Your grade is B.')
+#     else:
+#         if score >= C_score:
+#             print('Your grade is C.')
+#         else:
+#             if score >= D_score:
+#                 print('Your grade is D.')
+#             else:
+#                 print('Your grade is F.')
+# # Q5
+
+#Gather input
+# amount1 = int(input("Give me the first value: "))
+# amount2 = int(input("Give me the second value: "))
+# #determan they fit the rules
+# if amount1> 10:
+#     if amount2 < 100:
+# #determan whats the bigger value
+#         if amount1 > amount2:
+#             print(amount1)
+#         elif amount2 > amount1:
+#             print(amount2)
+#         elif amount2 == amount1:
+#             print(f"Amount 1 is {amount1} is queal to amount 2 {amount2}")
+#
+#
+
+# Q6
+#Gather speed
+# speed = int(input("What is the speed? "))
+#evaluate speed
+# if speed >= 24 and speed <= 56:
+#     print("Speed is normal. ")
+# else:
+#     print("Speed isnt normal.")
+# Q7
+#gather points
+# point = int(input("What is the point?"))
+#make sure values follows the rules then print the results
+# if point >= 9 and point <= 51:
+#     print("Valid points")
+# else:
+#     print("Invalid points")
